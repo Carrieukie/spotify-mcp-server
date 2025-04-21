9c1aabfad94958311b210a0f1d12e325d92ff219
Eric Kariuki <kariuki931@gmail.com>
2025-04-21 07:29:57 +0300

    Remove duplicate Ktor plugin and Kover version declarations in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
+    id("io.ktor.plugin") version "3.1.1"
+    id("org.jetbrains.kotlinx.kover") version "0.9.1" // or latest
     kotlin("jvm") version "2.1.10"
     kotlin("plugin.serialization") version "2.1.10"
-    id("io.ktor.plugin") version "3.1.1"
     application
 }
 
